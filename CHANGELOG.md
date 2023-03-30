9f1b6ffef3a564d2c21e191a738cb3455c787c51
TierneyJohn <Tierney-John@hotmail.com>
2023-03-30 08:46:11 +0800

    [modify] refs#PUERTS-T-1 PuerTS-MCS-项目初始化

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG
+
+## [0.1.0] - 2023-03-30
+
+### Features
+
+* [[PUERTS-T-1](https://breakthecontinent.jetbrains.space/p/puerts/issues/1)] PuerTS-MCS-项目初始化
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PuerTS_MCS")]
+[assembly: AssemblyDescription("PuerTS觅长生嵌入方案")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PuerTS_MCS")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4B4DF6DC-8EF2-4865-8C75-F28F6D33ACBC")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
